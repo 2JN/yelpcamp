f1a7538e2097825471f0fdde9a581936ec0b56e3
Jose Navas <jesquivelnavas@gmail.com>
2018-04-27 08:30:35 -0600

    Added comments and installed authentication packages

--- app.js
+++ app.js
@@ -17,10 +17,10 @@ var dbLink = process.env.MONGODB_URI ||
 mongoose.connect(dbLink)
 
 app.use(express.static(path.join(__dirname, 'public')))
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', function (req, res) {
-  res.render('index')
+  res.render('campgrounds/index')
 })
 
 app.get('/campgrounds', function (req, res) {
@@ -45,7 +45,7 @@ app.post('/campgrounds', function (req, res) {
 })
 
 app.get('/campgrounds/new', function (req, res) {
-  res.render('new')
+  res.render('campgrounds/new')
 })
 
 app.get('/campgrounds/:id', function (req, res) {
@@ -54,7 +54,37 @@ app.get('/campgrounds/:id', function (req, res) {
     if (err) {
       console.log(err)
     } else {
-      res.render('show', { campground })
+      res.render('campgrounds/show', { campground })
+    }
+  })
+})
+
+app.get('/campgrounds/:id/comments/new', function (req, res) {
+  Campground.findById(req.params.id, function (err, campground) {
+    if (err) {
+      console.log(err)
+    } else {
+      res.render('comments/new', { campground })
+    }
+  })
+})
+
+app.post('/campgrounds/:id/comments', function (req, res) {
+  Campground.findById(req.params.id, function (err, campground) {
+    if (err) {
+      console.log(err)
+      res.redirect('/campgrounds')
+    } else {
+      Comment.create(req.body.comment, function (err, comment) {
+        if (err) {
+          console.log(err)
+        } else {
+          campground.comments.push(comment)
+          campground.save()
+
+          res.redirect(`/campgrounds/${req.params.id}`)
+        }
+      })
     }
   })
 })
